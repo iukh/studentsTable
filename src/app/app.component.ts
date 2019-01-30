6c1119f87eb64b59f30da75d5fa6fb7bd11a977f
iukh <33911363+iukh@users.noreply.github.com>
2019-01-30 03:13:49 +0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Student } from './student';
 import { StudentsService } from './students.service';
-import { FormsModule } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { StudentsFilterPipe } from './students-filter.pipe';
 
 @Component({
@@ -19,41 +20,94 @@ export class AppComponent implements OnInit {
   tmpSearchInput: string = "";
   gpaMin: number;
   gpaMax: number;
+  isFormActive: boolean = false;
+  studentForm: FormGroup;
+  currentStudentIndex: number = null;
 
-  constructor(private studentsService: StudentsService) { }
-  ngOnInit() {
-    this.students = this.studentsService.getStudents();
+  initForm() {
+    this.studentForm = this.fb.group({
+      name: this.fb.group({
+        firstName: null,
+        lastName: null,
+        middleName: null
+      }),
+      dateOfBirthday: null,
+      gradePointAverage: null,
+      isActive: "true"
+    });
   }
-  changeSwitchValue() {
-    this.switchEnabled = !this.switchEnabled;
-  }
-  showPopUp(student) {
-    this.currentStudent = student;
-    this.isPopUpActive = true;
-  }
-  closePopUp() {
-    this.isPopUpActive = false;
-  }
-  deleteStudent() {
-    console.log(this.currentStudent);
-    this.currentStudent.isActive = false;
-    this.closePopUp();
-  }
-  studentSearch() {
-    this.searchInput =  this.tmpSearchInput;
-  }
-  private compareGPA(personA, personB) {
-    return personB.gradePointAverage - personA.gradePointAverage;
-  }
-  sortByGPA() {
-    this.students.sort(this.compareGPA);
-    this.gpaMin = this.students[this.students.length - 1].gradePointAverage;
-    this.gpaMax = this.students[0].gradePointAverage;
-  }
-  private compareLastName(personA, personB) {
-    return personA.lastName.localeCompare(personB.lastName);
-  }
-  sortByLastName() {
-    this.students.sort(this.compareLastName);
-  }
-}
+
+  constructor(
+    private studentsService: StudentsService,
+    private fb: FormBuilder) { }
+    ngOnInit() {
+      this.students = this.studentsService.getStudents();
+      this.initForm();
+    }
+    changeSwitchValue() {
+      this.switchEnabled = !this.switchEnabled;
+    }
+    showPopUp(student) {
+      this.currentStudent = student;
+      this.isPopUpActive = true;
+    }
+    closePopUp() {
+      this.isPopUpActive = false;
+    }
+    deleteStudent() {
+      console.log(this.currentStudent);
+      this.currentStudent.isActive = false;
+      this.closePopUp();
+    }
+    studentSearch() {
+      this.searchInput =  this.tmpSearchInput;
+    }
+    private compareGPA(personA, personB) {
+      return personB.gradePointAverage - personA.gradePointAverage;
+    }
+    sortByGPA() {
+      this.students.sort(this.compareGPA);
+      this.gpaMin = this.students[this.students.length - 1].gradePointAverage;
+      this.gpaMax = this.students[0].gradePointAverage;
+    }
+    private compareLastName(personA, personB) {
+      return personA.lastName.localeCompare(personB.lastName);
+    }
+    sortByLastName() {
+      this.students.sort(this.compareLastName);
+    }
+    activateForm() {
+      this.isFormActive =  !this.isFormActive;
+      this.gpaMax = 5;
+    }
+    onSubmit() {
+      this.currentStudent = new Student(this.studentForm.value.name.firstName,
+        this.studentForm.value.name.lastName,
+        this.studentForm.value.name.middleName,
+        this.studentForm.value.dateOfBirthday,
+        this.studentForm.value.gradePointAverage,true);
+        if (this.currentStudentIndex) {
+          this.students[this.currentStudentIndex] = this.currentStudent;
+          this.isFormActive = false;
+        } else {
+          this.students.push(this.currentStudent);
+        }
+        this.gpaMax = this.gpaMax + 1;
+        this.studentForm.reset();
+        this.currentStudentIndex = null;
+      }
+      studentEdit(student, i) {
+        this.activateForm();
+        this.currentStudentIndex = i;
+        this.studentForm.setValue({
+          name: {
+            firstName: student.firstName,
+            lastName: student.lastName,
+            middleName: student.middleName
+          },
+          dateOfBirthday: student.dateOfBirthday,
+          gradePointAverage: student.gradePointAverage,
+          isActive: "true"
+        });
+      }
+    }
